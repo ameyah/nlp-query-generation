ee3a282a5072b8950bce11b23977dd15a365dbbb
Ameya <ameyah@live.com>
2016-11-19 12:20:33 -0800

    Basic layout done. User preferences taken

---
+++ restaurant-finder/js/scripts.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ameya on 11/19/16.
+ */
+
+$(document).ready(function () {
+    /*$("#content").animate({top:'100px'}, "slow");
+    console.log('sds');
+
+    $("#button").click(function () {
+        $("#content").animate({left:'-105px'}, 5000);
+    })*/
+
+    $("#start").click(function () {
+        $("#welcome-content").hide(1000);
+        $("#preferences-form").show(1000);
+    })
+
+});
+
+$( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+} );
