265d23cf045b862f5801b1cfd9a680031ab7193a
HARSHIL RAJESH SHAH <shahharshil46@HARSHILs-MacBook-Pro.local>
2016-11-19 16:29:32 -0800

    NLU Classification based approach moved to nlu folder

---
+++ code/nlu/main_demo.py
@@ -0,0 +1,16 @@
+'''
+Created on 17-Nov-2016
+
+@author: shahharshil46
+'''
+import global_project_settings
+import NLU_Classification
+
+
+global_project_settings.init()
+
+# NLU_Classification.NLU_Classifier_Initiate()
+NLU_Classification.NLU_Classifier_Test(global_project_settings.classifier)
+
+st = "cheap restaurants and average"
+print(NLU_Classification.getLabel(global_project_settings.classifier, st))
