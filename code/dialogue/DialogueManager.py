b6786b0d421c10fc1427c7dc1f64c315670d9914
Rakesh Bora <rakeshvb@usc.edu>
2016-11-27 19:13:53 -0800

    NLU classification dialogue manager fixed

--- code/dialogue/DialogueManager.py
+++ code/dialogue/DialogueManager.py
@@ -13,6 +13,7 @@ from nlg import confirm_action
 from nlg import request_info as request_info_nlg
 from nlg import response_action
 from include import utils
+from include import db_conn
 
 
 class DialogueManager:
@@ -36,6 +37,7 @@ class DialogueManager:
         }
         self.confirm_flag = False
         self.current_confirm_factor = None
+        self.db_connection = db_conn.DbConnection()
 
     def store_preferences(self, preferences_priorities_dict):
         for preference in preferences_priorities_dict['preferences']:
@@ -54,7 +56,9 @@ class DialogueManager:
                 all_confirmed = self.is_all_confirmed()
                 if all_confirmed:
                     main_results = self.get_results(self.request_info_dict)
+                    print self.request_info_dict
                     tradeoff_factor = self.check_tradeoff_factor(self.request_info_dict, self.priorities)
+                    print tradeoff_factor
                     tradeoff_results = None
                     if tradeoff_factor:
                         tradeoff_request_dict = {}
@@ -62,16 +66,14 @@ class DialogueManager:
                             tradeoff_request_dict[key] = self.request_info_dict[key]
                         tradeoff_request_dict[tradeoff_factor] = self.preferences[tradeoff_factor]
                         tradeoff_results = self.get_results(tradeoff_request_dict)
-                    result_str = response_action.response_action(main_results, tradeoff_results)
+                    result_str = response_action.response_action(main_results, tradeoff_factor, tradeoff_results)
                     self.confirm_flag = False
                     return {"text": result_str, "main_results": main_results, "tradeoff_results": tradeoff_results}
             elif nlu_info_array[1] == "no":
                 self.confirm_flag = False
                 self.userDictionaryConfirmConfirm[self.current_confirm_factor] = None
                 self.request_info_dict[self.current_confirm_factor] = None
-        print self.request_info_dict
         self.request_info_dict = self.compare_dictionaries(self.request_info_dict, nlu_info_array)
-        print self.request_info_dict
         self.request_info_dict = self.set_preferences(self.request_info_dict)
         print self.request_info_dict
         while True:
@@ -95,6 +97,8 @@ class DialogueManager:
 
     def compare_dictionaries(self, main_data, new_data):
         try:
+            if new_data[0] == 'confirm':
+                return main_data
             main_data[new_data[0]] = new_data[1]
             self.userDictionaryConfirmConfirm[new_data[0]] = False
             return main_data
@@ -120,7 +124,8 @@ class DialogueManager:
     def check_tradeoff_factor(self, request_info_dict, priorities):
         for priority in priorities:
             if request_info_dict[priority] != self.preferences[priority]:
-                return priority
+                if self.preferences[priority]:
+                    return priority
 
     """
     def check_for_confirm_value(self, main_dic):
@@ -136,20 +141,15 @@ class DialogueManager:
                 return False
         return True
 
-    @staticmethod
-    def get_results(request_info_dict):
-        credentials = utils.dbcredentials(os.path.abspath(__file__))
-        connection = MySQLdb.connect(host=credentials["host"],  # your host, usually localhost
-                                     user=credentials["user"],  # your username
-                                     passwd=credentials["password"],  # your password
-                                     db=credentials["db"],
-                                     cursorclass=MySQLdb.cursors.SSDictCursor)  # stores result in the server. records as dict
-        query = "SELECT name, location, rating, cuisine, price, distance from restaurants where cuisine = {} and \
-                distance = {} and price = {}"
-        query = query.format(request_info_dict['cuisine'], request_info_dict['distance'], request_info_dict['price'])
+    def get_results(self, request_info_dict):
+        print request_info_dict
+        query = "SELECT name, location, rating, cuisine, price, distance from restaurants where cuisine = '{}' and \
+                distance = '{}' and price = '{}' ORDER BY rating DESC"
+        query = query.format(request_info_dict['cuisine'].title(), request_info_dict['distance'].upper(), request_info_dict['price'].upper())
+        cursor = self.db_connection.get_cursor()
         print "Query:" + query
-        cursor = connection.cursor()
         cursor.execute(query)
         response = cursor.fetchall()
+        print response
         cursor.close()
         return response
